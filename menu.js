38dfbcf313b1d7581c8a6bb46069f11a12cdee5a
Orville Malijan <orville@creativiumdigital.com>
2023-08-05 15:21:02 +1000

    v.1.2

--- menu.js
+++ menu.js
@@ -3,7 +3,7 @@ const grilled = [
     id: 1,
     name: 'Pork Barbeque',
     price: 25,
-    image: '/assets/images/menu/grilled/pork-bbq.jpg',
+    image: '/assets/images/menu/grilled/pork_bbq.jpg',
     description: '',
   },
   {
@@ -70,3 +70,145 @@ const grilled = [
     description: '',
   },
 ];
+
+const fried = [
+  {
+    id: 11,
+    name: 'French Fries',
+    price: 25,
+    image: '/assets/images/menu/fried/french-fries.jpeg',
+    description: '',
+  },
+  {
+    id: 12,
+    name: 'Fishball',
+    price: 25,
+    image: '/assets/images/menu/fried/fishball.jpeg',
+    description: '',
+  },
+  {
+    id: 13,
+    name: 'Squidball',
+    price: 25,
+    image: '/assets/images/menu/fried/squidball.jpeg',
+    description: '',
+  },
+  {
+    id: 14,
+    name: 'Kikiam',
+    price: 25,
+    image: '/assets/images/menu/fried/kikiam.jpeg',
+    description: '',
+  },
+  {
+    id: 15,
+    name: 'Siomai',
+    price: 20,
+    image: '/assets/images/menu/fried/siomai.jpeg',
+    description: '',
+  },
+  {
+    id: 16,
+    name: 'Dumplings',
+    price: 25,
+    image: '/assets/images/menu/fried/dumplings.jpeg',
+    description: '',
+  },
+  {
+    id: 17,
+    name: 'Cheese Sticks',
+    price: 18,
+    image: '/assets/images/menu/fried/cheese-sticks.jpg',
+    description: '',
+  },
+];
+
+const drinks = [
+  {
+    id: 18,
+    name: 'Softdrinks (mismo)',
+    price: 15,
+    image: '/assets/images/menu/drinks/mismo.jpeg',
+    description: '',
+  },
+  {
+    id: 19,
+    name: 'Gulaman',
+    price: 18,
+    image: '/assets/images/menu/drinks/gulaman.jpeg',
+    description: '',
+  },
+  {
+    id: 20,
+    name: 'C2',
+    price: 18,
+    image: '/assets/images/menu/drinks/c2.jpeg',
+    description: '',
+  },
+  {
+    id: 21,
+    name: 'Pale Pilsen',
+    price: 60,
+    image: '/assets/images/menu/drinks/pale-pilsen.jpeg',
+    description: '',
+  },
+  {
+    id: 22,
+    name: 'San Mig Light',
+    price: 65,
+    image: '/assets/images/menu/drinks/sanmig-light.jpeg',
+    description: '',
+  },
+  {
+    id: 23,
+    name: '1.5L Softdrinks',
+    price: 80,
+    image: '/assets/images/menu/drinks/coke1.5.jpeg',
+    description: '',
+  },
+];
+
+function createMenuItem(item) {
+  console.log(item);
+  const parentEl = document.createElement('div');
+  parentEl.classList.add('menu-item');
+
+  const itemImage = document.createElement('img');
+  itemImage.classList.add('menu-item__image');
+  itemImage.src = item.image;
+
+  parentEl.appendChild(itemImage);
+
+  const itemText = document.createElement('div');
+  itemText.classList.add('menu-item__text');
+
+  const itemTitle = document.createElement('span');
+  itemTitle.classList.add('menu-item__text--name');
+  itemTitle.innerText = item.name;
+
+  const itemPrice = document.createElement('span');
+  itemPrice.classList.add('menu-item__text--price');
+  itemPrice.innerHTML = `&#8369 ${item.price}`;
+
+  itemText.appendChild(itemTitle);
+  itemText.appendChild(itemPrice);
+
+  parentEl.appendChild(itemText);
+
+  return parentEl;
+}
+
+const grilledContainer = document.getElementById('grilled');
+const grilledItems = grilled.map((item) => createMenuItem(item));
+grilledContainer.innerHTML = '';
+grilledItems.forEach((item) => grilledContainer.appendChild(item));
+
+const friedContainer = document.getElementById('fried');
+const friedItems = fried.map((item) => createMenuItem(item));
+friedContainer.innerHTML = '';
+friedItems.forEach((item) => friedContainer.appendChild(item));
+
+const drinksContainer = document.getElementById('drinks');
+const drinksItems = drinks.map((item) => createMenuItem(item));
+drinksContainer.innerHTML = '';
+drinksItems.forEach((item) => drinksContainer.appendChild(item));
--- script.js
+++ script.js
@@ -3,120 +3,119 @@
 const imgFileNames = [];
 
 function populateImgFiles() {
-    for (let i = 20; i < 90; i++) {
-        imgFileNames.push(`img_0${i}.jpg`);
-    }
+  for (let i = 20; i < 90; i++) {
+    imgFileNames.push(`img_0${i}.jpg`);
+  }
 }
 
 function addImages() {
-    const galleryBox1 = document.querySelector(".gallery-row--1");
-    const galleryBox2 = document.querySelector(".gallery-row--2");
-    // Loop through the image filenames array
-    for (let i = 0; i < imgFileNames.length; i++) {
-        const container = document.createElement("div");
-        container.classList.add("image-container");
+  const galleryBox1 = document.querySelector('.gallery-row--1');
+  const galleryBox2 = document.querySelector('.gallery-row--2');
+  // Loop through the image filenames array
+  for (let i = 0; i < imgFileNames.length; i++) {
+    const container = document.createElement('div');
+    container.classList.add('image-container');
 
-        const image = document.createElement("img");
-        image.src = "/assets/images/gallery/customers/" + imgFileNames[i];
-        image.classList.add("gallery__image");
-        image.setAttribute("draggable", "false");
+    const image = document.createElement('img');
+    image.src = '/assets/images/gallery/customers/' + imgFileNames[i];
+    image.classList.add('gallery__image');
+    image.setAttribute('draggable', 'false');
 
-        //put the first half of the images to galleryBox1
-        if (i < imgFileNames.length / 2) {
-            galleryBox1.appendChild(container);
-            container.appendChild(image);
-        } else {
-            //put the second half of the images to galleryBox2
-            galleryBox2.appendChild(container);
-            container.appendChild(image);
-        }
+    //put the first half of the images to galleryBox1
+    if (i < imgFileNames.length / 2) {
+      galleryBox1.appendChild(container);
+      container.appendChild(image);
+    } else {
+      //put the second half of the images to galleryBox2
+      galleryBox2.appendChild(container);
+      container.appendChild(image);
     }
+  }
 }
 
 populateImgFiles();
 addImages();
 
 //SMOOTH SCROLLING
-const navItems = document.querySelectorAll(".navigation__item");
+const navItems = document.querySelectorAll('.navigation__item');
 
 function smoothScroll() {
-    navItems.forEach(function (link) {
-        link.addEventListener("click", function (e) {
-            e.preventDefault();
-            const href = link.getAttribute("href");
+  navItems.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const href = link.getAttribute('href');
 
-            //scroll back to top
-            if (href === "#home") {
-                window.scrollTo({
-                    top: 0,
-                    behavior: "smooth"
-                });
-            }
-            if (href !== "#home") {
-                const sectionEl = document.querySelector(href);
-                sectionEl.scrollIntoView({
-                    behavior: "smooth"
-                });
-            }
+      //scroll back to top
+      if (href === '#home') {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
         });
+      }
+      if (href !== '#home') {
+        const sectionEl = document.querySelector(href);
+        sectionEl.scrollIntoView({
+          behavior: 'smooth',
+        });
+      }
     });
+  });
 }
 smoothScroll();
 
 //NAV ANIMATION
-const hamburger = document.querySelector("#hamburger");
+const hamburger = document.querySelector('#hamburger');
 
 function toggleClassDelay(el, classList) {
-    const stack = [...el].reverse();
-    stack.forEach((el, index) => {
-        setTimeout(() => {
-            el.classList.toggle(classList);
-        }, index * 100);
-    });
+  const stack = [...el].reverse();
+  stack.forEach((el, index) => {
+    setTimeout(() => {
+      el.classList.toggle(classList);
+    }, index * 100);
+  });
 }
 
 function toggleHamburger() {
-    const navList = document.querySelector('.navigation__list');
-    const spans = document.querySelectorAll("#span1, #span2, #span3");
-    const navParent = document.querySelector(".navigation");
-    spans.forEach((span) => {
-        span.classList.toggle("open");
-    });
-    navParent.classList.toggle("open");
-    navList.classList.toggle("open");
-
+  const navList = document.querySelector('.navigation__list');
+  const spans = document.querySelectorAll('#span1, #span2, #span3');
+  const navParent = document.querySelector('.navigation');
+  spans.forEach((span) => {
+    span.classList.toggle('open');
+  });
+  navParent.classList.toggle('open');
+  navList.classList.toggle('open');
 }
 
-hamburger.addEventListener("click", () => {
-    toggleHamburger();
-    toggleClassDelay(navItems, "items-open");
+hamburger.addEventListener('click', () => {
+  toggleHamburger();
+  toggleClassDelay(navItems, 'items-open');
 });
 
 navItems.forEach((item) => {
-    item.addEventListener("click", () => {
-        toggleHamburger();
-        toggleClassDelay(navItems, "items-open");
-        smoothScroll();
-    });
+  item.addEventListener('click', () => {
+    toggleHamburger();
+    toggleClassDelay(navItems, 'items-open');
+    smoothScroll();
+  });
 });
 
 //sticky nav
-const header = document.querySelector(".header");
+const header = document.querySelector('.header');
 
 function callback(entries) {
-    const ent = entries[0];
+  const ent = entries[0];
 
-    if (!ent.isIntersecting) {
-        document.body.classList.add("sticky");
-    }
-    if(ent.isIntersecting) {
-        document.body.classList.remove("sticky");
-    }
+  if (!ent.isIntersecting) {
+    document.body.classList.add('sticky');
+  }
+  if (ent.isIntersecting) {
+    document.body.classList.remove('sticky');
+  }
 }
 const options = {
-    root: null,
-    threshold: 0,
-    rootMargin: '-80px'
+  root: null,
+  threshold: 0,
+  rootMargin: '-80px',
 };
 const obs = new IntersectionObserver(callback, options);
 
@@ -124,6 +123,6 @@ obs.observe(header);
 
 //copyright date
 const currentYear = new Date().getFullYear();
-const copyrightYear = document.getElementById("copyright-year");
+const copyrightYear = document.getElementById('copyright-year');
 
 copyrightYear.textContent = currentYear;
